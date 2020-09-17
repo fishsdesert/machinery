9c775a510bd0ffcf4e98b9674401fa212e97d9ed
“kui <haobaikui163@163.com>
2020-09-17 17:23:07 +0800

    修改

---
+++ src/main/java/com/example/mechanical/service/MacSysNodeService.java
@@ -0,0 +1,8 @@
+package com.example.mechanical.service;
+
+/**
+ * @author HaoBaiKui
+ * @date 2020/9/17 17:20
+ */
+public interface MacSysNodeService {
+}
---
+++ src/main/java/com/example/mechanical/service/impl/MacSysNodeServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.mechanical.service.impl;
+
+import com.example.mechanical.service.MacSysNodeService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author HaoBaiKui
+ * @date 2020/9/17 17:20
+ */
+@Service
+public class MacSysNodeServiceImpl implements MacSysNodeService {
+}
