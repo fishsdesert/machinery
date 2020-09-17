ea197762e6e161a3efc34c3d49ced26546e473d4
“kui <haobaikui163@163.com>
2020-09-17 17:07:54 +0800

    修改

---
+++ src/main/java/com/example/mechanical/service/impl/MacSysRecoldServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.mechanical.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author HaoBaiKui
+ * @date 2020/9/17 17:04
+ */
+@Service
+public class MacSysRecoldServiceImpl {
+}
