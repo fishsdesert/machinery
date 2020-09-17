568b9f89f23899f8ea6026ccd201af1aa9b025a4
“kui <haobaikui163@163.com>
2020-09-17 17:01:47 +0800

    修改

---
+++ src/main/java/com/example/mechanical/service/impl/MacSysRoleServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.mechanical.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author HaoBaiKui
+ * @date 2020/9/17 16:56
+ */
+@Service
+public class MacSysRoleServiceImpl {
+}
